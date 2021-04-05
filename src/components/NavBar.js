271450805622324db640ca403e9f6b0b218494cf
Kate Lewis <lewiskaitlynj@gmail.com>
2021-04-05 15:24:35 -0400

    added zip code field

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,20 +5,16 @@ import melpLogo from './melpLogo.png'
 export function NavBar() {
     return (
         <div className={styles['nav-bar']}>
-            <div className={styles.left}>
+    
                 <div className="field has-addons">
                     
                         <img src={melpLogo} alt="Melp Logo" width="150" />
-                        
-                        <button className="button is-static is-medium">Search</button>
-                   
-                   
+                                                            
+                        <input className={styles.zipCode} type="text" placeholder="Zip Code" />
                         <input className={styles.input} type="text" placeholder="Japanese, Mediterranean, Italian..." />
-                 
-    
-                </div>
-            </div>
-            <div className={styles.right}>
+        
+                        <button className={styles.button}>Search</button>
+         
                 <button> Login </button>
                 <button> Sign Up </button>
                 <button> About Us </button>
