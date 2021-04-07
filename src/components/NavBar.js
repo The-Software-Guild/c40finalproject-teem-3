5a1702054445e37e8726fb52c4f433f7235f6e43
Ant1997 <anthonyhuynh997@gmail.com>
2021-04-07 12:52:21 -0500

    Update NavBar.js (Storing user input Zipcode|Category)
    When user put in zipcode and category and then either press ENTER or CLICK search button, it will print in console what the user had entered.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './NavBar.module.css'
 import melpLogo from './melpLogo.png'
 
-export function NavBar() {
+export function NavBar(props) {
+
+    const [term, setTerm] = useState(props.term || '');
+    const [location, setLocation] = useState(props.location || '');
+
+    function submit(e){
+        console.log(term, location);
+        e.preventDefault();
+    }
+
     return (
-        <div className={styles['nav-bar']}>
+        <form className={styles['nav-bar']} onSubmit={submit}>
     
                 <div className="field has-addons">
                     
                         <img src={melpLogo} alt="Melp Logo" width="150" />
                                                             
-                        <input className={styles.zipCode} type="text" placeholder="Zip Code" />
-                        <input className={styles.input} type="text" placeholder="Japanese, Mediterranean, Italian..." />
+                        <input className={styles.zipCode} 
+                            onChange={(e) => setLocation(e.target.value)}
+                            type="text" 
+                            placeholder="Zip Code" />
+
+                        <input className={styles.input} 
+                            onChange={(e) => setTerm(e.target.value)}
+                            type="text" 
+                            placeholder="Japanese, Mediterranean, Italian..." />
         
-                        <button className={styles.button}>Search</button>
+                        <button className={styles.button}
+                            onClick={submit}>Search</button>
          
                 <button> Login </button>
                 <button> Sign Up </button>
@@ -21,7 +38,7 @@ export function NavBar() {
                 <button><a href="/Home">Home </a></button>       
         
             </div>
-        </div>
+        </form>
 
     );
 }
