e34430beffd13716bd8f15129dbe74c3715da7d8
Ant1997 <anthonyhuynh997@gmail.com>
2021-04-07 09:05:14 -0500

    Search Page Header

--- src/pages/SearchPage/SearchResultSummary.js
+++ src/pages/SearchPage/SearchResultSummary.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import styles from './SearchResultSummary.module.css'
 
 export function SearchResultsSummary(){
     return(
-        <div>
-            <h1 className='subtitle'>
-                <strong> burgers </strong>
-                berlin
-            </h1>
+        <div className={styles['container']}> 
+            <div className={styles['search-summary']}>
+                <p id="search_Category" className="subtitle">
+                    Japanese, <strong> Texas </strong>
+                
+                </p>
+                <p>
+                    Showing 1-20 out of 543 results
+                </p>
+            </div>
+            <div className={styles.filters}>
+                
+            </div>
+
         </div>
     )
 }
---
+++ src/pages/SearchPage/SearchResults/SearchResults.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function SearchResults() {
+    return(
+        <div>
+            Search Results
+        </div>
+    );
+}
