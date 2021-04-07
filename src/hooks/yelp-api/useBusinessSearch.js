3cc4f88702ab8166be79926e3ba4c0c4a65a36fc
Ant1997 <anthonyhuynh997@gmail.com>
2021-04-07 11:30:28 -0500

    Update useBusinessSearch.js

--- src/hooks/yelp-api/useBusinessSearch.js
+++ src/hooks/yelp-api/useBusinessSearch.js
@@ -1,5 +1,5 @@
-import {useState} from 'react';
-
+import {useState, useEffect} from 'react';
+import * as api from './api';
 
 export function useBusinessSearch(term, location){
     const [businesses, setBusinesses] = useState([]);
@@ -8,5 +8,17 @@ export function useBusinessSearch(term, location){
 
     useEffect(()=> {
         setBusinesses([]);
-    })
+        const fetchData = async () =>{
+            try{
+                const rawData = await api.get('/businesses/search', searchParams);
+                const resp = await rawData.json();
+                setBusinesses(resp.businesses);
+                setAmountResults(resp.total); 
+            }catch(e){
+                console.error(e);
+            }
+        };
+        fetchData();
+    }, [searchParams]);
+    return [businesses, amountResults,searchParams,setSearchParams];
 }
