a65830facd48ce06aaf89fa8bcd7e05b69384250
Ant1997 <anthonyhuynh997@gmail.com>
2021-04-08 12:25:33 -0500

    Search Page Styling

--- src/pages/SearchPage/SearchResults/SearchResult.js
+++ src/pages/SearchPage/SearchResults/SearchResult.js
@@ -8,20 +8,25 @@ export function SearchResult(props) {
     }
 
     //const tags = props.business.categories.map(category => <p key={props.business.id + category.title}> {category.title} </p>);
-    const addressLines = props.business.location.display_address.map(addressLine => <p key={props.business.id + addressLine}> {addressLine} </p>);
+    const addressLines = props.business.location.display_address.map(addressLine => <span key={props.business.id + addressLine}> {addressLine} </span>);
     return(
         <div className={styles['search-result']}>
             <div className={styles['business-image']}>
                 <img className={styles['business-image']} src={props.business.image_url} alt='business'/>
             </div>
             <div className={styles['business-info']}>
-                <h2>{props.business.name} </h2>
+                <strong>{props.business.name} </strong>
+               
+            </div>
+            <div className={styles['business-info']}>
                 <p>Price Range: {props.business.price}</p> 
                 <p>Rating: {props.business.rating}</p>
                 <p>Total Reviews: {props.business.review_count}</p>
-                <p>{props.business.phone}</p>
-                {addressLines}
                
+            </div>
+            <div className={styles['business-address']}>
+                <p>{props.business.phone}</p>
+                <p> {addressLines} </p>
             </div>
         </div>
     );
