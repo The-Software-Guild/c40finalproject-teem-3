33e5fb7ffb0f86c5eb254826fe8e009f7dfd15af
Ant1997 <anthonyhuynh997@gmail.com>
2021-04-08 13:48:14 -0500

    Dot Appears... just no longitude/lat

--- src/pages/hooks/yelp-api/api.js
+++ src/pages/hooks/yelp-api/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export function get(path, queryParams) {
     const query = queryString.stringify(queryParams);
     let fetchURL = "/v3" + path + query;
-    let API_Key = ''; //API KEY HERE
+    let API_Key = 'TN1_Qg2qSyxlLk1bkJvfn7rWM57xfDyHSVrC90jfpRAB6hiJ74bbBwVofqYc9vTJJwjt0JyzgfIfjp-TxMvkIdz5EcQlAcaQ3E8u3fQruTvDYBjfXsW_UsVe2CxvYHYx'; //API KEY HERE
     let bearer = 'Bearer ' + API_Key;
     console.log(fetchURL);
 
