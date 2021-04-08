40ddbcd658ee19ec8365d287d01207c716b456bb
Ant1997 <anthonyhuynh997@gmail.com>
2021-04-08 10:33:59 -0500

    Update api.js

--- src/pages/hooks/yelp-api/api.js
+++ src/pages/hooks/yelp-api/api.js
@@ -4,12 +4,14 @@ import axios from 'axios';
 export function get(path, queryParams) {
     const query = queryString.stringify(queryParams);
     let fetchURL = "/v3" + path + query;
+    let API_Key = ''; //API KEY HERE
+    let bearer = 'Bearer ' + API_Key;
     console.log(fetchURL);
 
     let results = axios.get(fetchURL,
     {
         headers: {
-            'Authorization': 'Bearer GdR9jT6Lkxj12Kv0UwKPT9BRhIfaQ72XBlPVz-nUKfxJJs0xv0nfppFtKhrH1bbOJudbZTurAFg3sgJEFY1lq5-MvlXDNt-8x08GQ6nze4jYetFF7vCQFv1faGNuYHYx',
+            'Authorization': bearer,
             'Access-Control-Allow-Origin': '*',
             'Content-Type': 'application/json',
             "accepts":"application/json"
